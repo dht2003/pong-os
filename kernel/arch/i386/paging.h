157860eb962d07f4948289a1edf4dfc21cec2361
dht2003 <guysham2003@gmail.com>
2022-01-08 01:46:07 +0200

    paging

--- kernel/arch/i386/paging.h
+++ kernel/arch/i386/paging.h
@@ -1,10 +1,15 @@
 #ifndef PAGING_H
 #define PAGEING_H
 
+#include <stdint.h>
+#include <stdbool.h>
+#include <arch/i386/isr.h>
+
 #define TABLE_SIZE 1024
 #define DIRECTORY_SIZE 1024
+#define PAGE_SIZE 4096
 
-#include <stdint.h>
+
 
 typedef struct Page_entry_struct {
     uint32_t present : 1;
@@ -22,8 +27,18 @@ typedef struct Page_table_struct {
 } page_table_t;
 
 typedef struct Page_directory_struct {
-    page_table_t *directory[DIRECTORY_SIZE];
+    page_table_t *tables[DIRECTORY_SIZE];
+    uint32_t tablesPhysical[1024];
+    uint32_t physicalAddr;
 } page_directory_t;
+
+void init_paging();
+
+void switch_page_directory(page_directory_t *dir);
+
+page_entry_t *get_page(uint32_t addr,bool make,page_directory_t *dir);
+
+void page_fault(registers_t regs);
 
 
 #endif
